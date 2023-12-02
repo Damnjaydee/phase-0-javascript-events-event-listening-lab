83115deccdb061de0d971d3e4448c17d1562e1ae
jeffrey domond <noella@Noellas-iMac.lan1>
2023-12-02 17:49:44 -0500

    javascript events

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// index.js
+
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+  
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Call the outer function to execute addingEventListener() and activate the event listener
+  addingEventListener();
+  
